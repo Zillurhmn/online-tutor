8deffcb689174737f29352b800ccf007d5530c26
kaiesmahmud <kaiesmahmud@gmail.com>
2023-01-15 18:25:09 +0600

    login changed

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -33,6 +33,7 @@ const Login = () => {
     //     Cookies.set("expiry", res?.data?.expiry);
     //   navigate("/", { replace: true });
 // console.log("my form data is = ",data);
+// edit branch
 const res = getData(`studentlogin?email=${data?.email}&password=${data?.password}`);
  res.then(data => {
     setStudent(data.data)
